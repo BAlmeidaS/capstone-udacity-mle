8cda7c5f2a79a0371dbf28579fb4d9b08acec33c
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-11-16 23:03:40 -0300

    creating an iou module

---
+++ project/model_utils/iou.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def upper_left(v):
+    """upper_left
+    finds the lower right position based on an array cointaing in
+    each row four values:
+        [cx,cy,h,w]
+
+    Args:
+        v (np.array): each row [cx, cy, h, w] of a point
+
+    Returns:
+        np.array: array with x values of upper left
+        np.array: array with y values of upper left
+    """
+    return v[:, 0] - v[:, 2] / 2, v[:, 1] - v[:, 3] / 2
+
+
+def lower_right(v: np.array) -> (np.array, np.array):
+    """lower_right
+    finds the lower right position based on an array cointaing in
+    each row four values:
+        [cx,cy,h,w]
+
+    Args:
+        v (np.array): each row [cx, cy, h, w]
+
+    Returns:
+        np.array: array with x values of lower right
+        np.array: array with y values of lower right
+    """
+    return v[:, 0] + v[:, 2] / 2, v[:, 1] + v[:, 3] / 2
+
+
+def iou(a, b, epsilon=1e-7):
+    """ Given two arrays `a` and `b` where each row contains
+        a bounding box defined as a list of four numbers:
+            [cx,cy,h,w]
+        where:
+            cx represents center of bounding box in axis x
+            cy represents center of bounding box in axis y
+            h represents the height of bounding box
+            w represents the width of bounding box
+        It returns the Intersect of Union scores for each corresponding
+        pair of boxes.
+
+    Args:
+        a:        (numpy array) each row containing [x1,y1,x2,y2]
+        b:        (numpy array) each row containing [x1,y1,x2,y2]
+
+    Returns: int:
+    """
+
+    a_ul = upper_left(a)
+    a_lr = lower_right(a)
+
+    b_ul = upper_left(b)
+    b_lr = lower_right(b)
+
+    # COORDINATES OF THE INTERSECTION BOXES
+    x1 = np.array([a_ul[0], b_ul[0]]).max(axis=0)
+    y1 = np.array([a_ul[1], b_ul[1]]).max(axis=0)
+
+    x2 = np.array([a_lr[0], b_lr[0]]).min(axis=0)
+    y2 = np.array([a_lr[1], b_lr[1]]).min(axis=0)
+
+    # AREAS OF OVERLAP - Area where the boxes intersect
+    width = (x2 - x1)
+    height = (y2 - y1)
+
+    # handle case where there is NO overlap
+    width[width < 0] = 0
+    height[height < 0] = 0
+
+    area_overlap = width * height
+
+    # COMBINED AREAS
+    area_a = a[:, 2] * a[:, 3]
+    area_b = b[:, 2] * b[:, 3]
+    area_combined = area_a + area_b - area_overlap
+
+    # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+    iou = area_overlap / (area_combined + epsilon)
+    return iou
---
+++ tests/model_utils/test_iou.py
@@ -0,0 +1,30 @@
+from project.model_utils.iou import iou
+import numpy as np
+from numpy import testing
+
+
+def test_iou_with_1_param():
+    result = iou(np.expand_dims(np.array([2.05, 2.05, .2, .2]), axis=0),
+                 np.expand_dims(np.array([2, 2, .2, .2]), axis=0))
+
+    testing.assert_array_almost_equal(result,
+                                      np.array([0.39130367]),
+                                      decimal=7)
+
+
+def test_iou_with_2_params():
+    result = iou(np.array([[2.05, 2.05, .2, .2], [1, 1, 3, 3]]),
+                 np.array([[2, 2, .2, .2], [1, 1, 3, 3]]))
+
+    testing.assert_array_almost_equal(result,
+                                      np.array([0.39130367, 1]),
+                                      decimal=7)
+
+
+def test_iou_without_intersection():
+    result = iou(np.expand_dims(np.array([2.05, 2.05, .2, .2]), axis=0),
+                 np.expand_dims(np.array([3, 3, .2, .2]), axis=0))
+
+    testing.assert_array_almost_equal(result,
+                                      np.array([0]),
+                                      decimal=7)
