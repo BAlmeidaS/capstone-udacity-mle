d5c6325036645cb8d4a1de6aa9a3718c870a6dcc
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-11-18 08:24:46 -0300

    changing smooth l1 to use tf functions instead numpy

--- project/model_utils/smooth.py
+++ project/model_utils/smooth.py
@@ -1,11 +1,12 @@
 import numpy as np
+import tensorflow as tf
 
 
 def smooth_l1(x: np.array) -> float:
-    absolute_loss = np.abs(x)
+    absolute_loss = tf.abs(x)
     square_loss = 0.5 * x ** 2
 
-    l1_loss = np.where(np.less(absolute_loss, 1.0),
+    l1_loss = tf.where(tf.less(absolute_loss, 1.0),
                        square_loss, absolute_loss - 0.5)
 
-    return np.sum(l1_loss)
+    return tf.reduce_sum(l1_loss)
