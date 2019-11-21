ed45efbe5e263b4c2d96fe552d614b503421b240
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-11-21 09:55:50 -0300

    minor changes on loss function

--- project/model/loss.py
+++ project/model/loss.py
@@ -5,20 +5,19 @@ from project.model.smooth_l1 import smooth_l1
 
 
 class SSDloss():
-    def __call__(self, y_true, y_pred):
-        return self.loc_loss(y_true, y_pred)
+    def loss(self, y_true, y_pred, *args):
+        loc = self.loc_loss(y_true, y_pred)
+
+        negatives = y_true[:, :, 0]
+        positives = tf.reduce_max(y_true[:, :, 1:-4], axis=-1)
+
+        loc_loss = tf.reduce_sum(loc * positives, axis=-1)
+
+        return tf.convert_to_tensor(loc_loss, dtype=tf.float32)
 
     def x_k(self, ground_truth, default_box):
         return iou > 0.5
 
     def loc_loss(self, y_true, y_pred):
         z = y_true[:, :, -4:] - y_pred[:, :, -4:]
-
-        smooth = smooth_l1(z)
-
-        negatives = y_true[:, :, 0]
-        positives = tf.reduce_max(y_true[:, :, 1:-4], axis=-1)
-
-        loc_loss = tf.reduce_sum(smooth * positives, axis=-1)
-
-        return loc_loss
+        return smooth_l1(z)
