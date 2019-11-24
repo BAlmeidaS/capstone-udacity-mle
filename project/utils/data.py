77fa26f5609c46c212179ee73e725e8ca2c04f5a
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-11-24 06:25:22 -0300

    a couple of useful functions in project utils

--- project/utils/data.py
+++ project/utils/data.py
@@ -18,10 +18,42 @@ def boats() -> pd.DataFrame:
     return all_train().query("LabelSemantic == 'Boat'")
 
 
-def df_bbox_reference(with_s2: list = [75, 39],
-                      with_s1: list = [11, 7, 5, 3]) -> pd.DataFrame:
-    """Get a df with all bbox defaults defined"""
-    def bbox(sizes, stride=1, ratios=None):
+def group_images(df: pd.DataFrame) -> pd.DataFrame:
+    """return a dataframe with all images grouped"""
+    imgs = df[['ImageID', 'Path']].drop_duplicates()
+    imgs.set_index('ImageID', inplace=True)
+
+    for c in ['cx', 'cy', 'w', 'h', 'bbox_ref']:
+        imgs = imgs.join(boats.groupby('ImageID')[c].apply(list))
+
+    imgs.reset_index(inplace=True)
+    return imgs
+
+
+class StandardBoudingBoxes:
+    def __init__(self):
+        self.set_references()
+
+    def set_references(self, with_s2: list = [75, 39],
+                       with_s1: list = [11, 7, 5, 3]) -> pd.DataFrame:
+        """Get a df with all bbox defaults defined"""
+
+        refs_s2 = self._bbox(with_s2, stride=2, ratios=[1/2])
+        refs_s1 = self._bbox(with_s1, stride=1, ratios=[1/2, 1/3])
+
+        self.references = (pd.concat([refs_s2, refs_s1])
+                             .reset_index()[['cx', 'cy', 'w', 'h']])
+
+    def match(self, bbox: pd.Series, iou_threshold: float = 0.5) -> np.array:
+        """return all bbox default that matches with some bbox passed"""
+        bbox = bbox[['cx', 'cy', 'w', 'h']]
+        ious = iou(bbox.values, self.references.values)
+        ious[ious < iou_threshold] = 0
+
+        bboxs = np.where((ious == np.amax(ious)) & (ious > 0))[0]
+        return bboxs
+
+    def _bbox(self, sizes, stride=1, ratios=None):
         arr = []
         for n in sizes:
             e = 1 / n
@@ -37,32 +69,3 @@ def df_bbox_reference(with_s2: list = [75, 39],
                     for r in ratios:
                         arr.append([e*(i+3/2), e*(j+3/2), t, t*r])
         return pd.DataFrame(arr, columns=['cx', 'cy', 'w', 'h'])
-
-    refs_s2 = bbox(with_s2, stride=2, ratios=[1/2])
-    refs_s1 = bbox(with_s1, stride=1, ratios=[1/2, 1/3])
-
-    return pd.concat([refs_s2, refs_s1]).reset_index()[['cx', 'cy', 'w', 'h']]
-
-BBOXES = df_bbox_reference()
-
-def match_bboxes(bbox: pd.Series, iou_threshold: float = 0.5,
-                 bbox_reference: pd.DataFrame = BBOXES) -> np.array:
-    """return all bbox default that matches with some bbox passed"""
-    bbox = bbox[['cx', 'cy', 'w', 'h']]
-    ious = iou(bbox.values, bbox_reference.values)
-    ious[ious < iou_threshold] = 0
-
-    bboxs = np.where((ious == np.amax(ious)) & (ious > 0))[0]
-    return bboxs
-
-
-def group_images(df: pd.DataFrame) -> pd.DataFrame:
-    """return a dataframe with all images grouped"""
-    imgs = df[['ImageID', 'Path']].drop_duplicates()
-    imgs.set_index('ImageID', inplace=True)
-
-    for c in ['cx', 'cy', 'w', 'h', 'bbox_ref']:
-        imgs = imgs.join(boats.groupby('ImageID')[c].apply(list))
-
-    imgs.reset_index(inplace=True)
-    return imgs
