13046551fcc3e7bcdbfd9096c661050cce6ecd99
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-11-27 10:34:08 -0300

    implementing hard negative mining

--- project/model/loss.py
+++ project/model/loss.py
@@ -14,24 +14,33 @@ class SSDloss():
             return tf.convert_to_tensor(0, dtype=tf.float32)
 
         loc = self.loc_loss(y_true, y_pred)
-        conf_loss = self.conf_loss(y_true, y_pred)
+        conf_loss_pos, amount_pos = self.conf_loss_pos(y_true, y_pred)
+        conf_loss_neg = self.conf_loss_neg(y_true, y_pred, amount_pos)
 
         positives = tf.reduce_max(y_true[:, :, 1:-4], axis=-1)
 
-        loc_loss = tf.reduce_sum(loc * tf.transpose(positives))
+        loc_loss = tf.reduce_sum(loc * positives)
 
-        loss = (conf_loss + 1 * loc_loss) / N
+        loss = (conf_loss_pos + conf_loss_neg + 1 * loc_loss) / N
 
         return tf.convert_to_tensor(loss, dtype=tf.float32)
 
     def loc_loss(self, y_true, y_pred):
-        z = y_true[:, :, -4:] - y_pred[:, :, -4:]
-        return smooth_l1(z)
+        z = y_pred[:, :, -4:] - y_true[:, :, -4:]
+        return tf.reduce_sum(smooth_l1(z), axis=-1)
 
-    def conf_loss(self, y_true, y_pred):
-        ln = self.log(y_pred)
-        return -tf.reduce_sum(y_true[:, :, :-4] * ln)
+    def conf_loss_neg(self, y_true, y_pred, amount_pos):
+        ln = -1 * y_true[:, :, 0] * self.log(y_pred[:, :, 0])
+        ln_flatten = tf.reshape(ln, (-1,))
+
+        top_k, _ = tf.math.top_k(ln_flatten, k=tf.cast((amount_pos * 3), tf.int32))
+
+        return tf.reduce_sum(top_k)
+
+    def conf_loss_pos(self, y_true, y_pred):
+        ln = -1 * y_true[:, :, 1:-4] * self.log(y_pred[:, :, 1:-4])
+        return (tf.reduce_sum(ln),
+                tf.math.count_nonzero(y_true[:, :, 1:-4]))
 
     def log(self, y_pred):
-        return tf.math.log(tf.math.maximum(y_pred[:, :, :-4],
-                                           1e-15))
+        return tf.math.log(tf.math.maximum(y_pred, 1e-15))
