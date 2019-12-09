e0b2b948bd082ec490b27b7624986af47c057c7b
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-12-09 14:29:16 -0300

    adding a script to union hdf5 preprocessed files

---
+++ project/utils/union_hdf5.py
@@ -0,0 +1,24 @@
+# imports
+import os
+
+import modin.pandas as pd
+
+import project.download_content as content
+
+from tqdm import tqdm
+
+
+def main():
+    tqdm.pandas()
+    modelpath = os.path.join(content.DATAPATH, "MODEL")
+
+    df = pd.concat([pd.read_hdf(os.path.join(modelpath, f"data_preprocessed_{i}.h5"),
+                                'X', mode='r') for i in range(24)])
+
+    filepath = os.path.join(modelpath, "data_preprocessed.h5")
+
+    df.to_hdf(filepath, key='X', mode='w')
+
+
+if __name__ == '__main__':
+    main()
