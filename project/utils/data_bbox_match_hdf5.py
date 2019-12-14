acdc4a8b04b1e4f8aa49fc97682d8c3eabe84875
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-12-14 14:48:26 -0300

    exposing, as public var, standard bboxes on utils data bbox match script

--- project/utils/data_bbox_match_hdf5.py
+++ project/utils/data_bbox_match_hdf5.py
@@ -21,6 +21,15 @@ import time
 tqdm.pandas()
 modelpath = os.path.join(content.DATAPATH, "MODEL")
 
+standard_bboxes = data.StandardBoudingBoxes(feature_map_sizes=[38, 19, 10, 5, 3, 1],
+                                            ratios_per_layer=[[1, 1/2, 2],
+                                                              [1, 1/2, 1/3, 2, 3],
+                                                              [1, 1/2, 1/3, 2, 3],
+                                                              [1, 1/2, 1/3, 2, 3],
+                                                              [1, 1/2, 2],
+                                                              [1, 1/2, 2]])
+
+
 
 def train_and_save_model(df):
     # train one hot encoding model
@@ -44,14 +53,6 @@ def load_model():
 @ray.remote
 def part_process(df, part):
     # define standard boxes (based on ssd architecture)
-    standard_bboxes = data.StandardBoudingBoxes(feature_map_sizes=[38, 19, 10, 5, 3, 1],
-                                                ratios_per_layer=[[1, 1/2, 2],
-                                                                  [1, 1/2, 1/3, 2, 3],
-                                                                  [1, 1/2, 1/3, 2, 3],
-                                                                  [1, 1/2, 1/3, 2, 3],
-                                                                  [1, 1/2, 2],
-                                                                  [1, 1/2, 2]])
-
     # setting bbox ref which match with standard bboxes designed
     df['bbox_ref'] = df.apply(standard_bboxes.match, axis=1)
 
