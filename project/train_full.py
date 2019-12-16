38501bac6f970cb7f3619e960dc5f41b3c39f128
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-12-16 02:58:17 -0300

    changing train full to use async version of data aug

--- project/train_full.py
+++ project/train_full.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
-import os
+import ray
+from collections import deque
 
 from project.utils import data_augmentation as da
 
@@ -8,14 +9,10 @@ from keras.optimizers import SGD
 
 import project.download_content as content
 from project.model.ssd_model_300_full import ssd_model_300
-from project.model.loss import SSDloss, BBOX_REF
+from project.model.loss import SSDloss
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
-
-DATAPATH = os.path.join(content.DATAPATH, "MODEL", "data_300_vgg.h5")
-
-ALL_ANCHORS = BBOX_REF.references.values
 
 
 def count_images():
@@ -23,7 +20,7 @@ def count_images():
 
     count_images = 0
 
-    with h5py.File(DATAPATH, 'r') as f:
+    with h5py.File(da.DATAPATH, 'r') as f:
         for group in groups:
             count_images += f[group]['images'][:].shape[0]
 
@@ -33,7 +30,6 @@ def count_images():
 def load_model():
     model = ssd_model_300()
 
-    # opt = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, amsgrad=False)
     opt = SGD(learning_rate=1e-3, momentum=0.9, nesterov=False)
     ssd_loss = SSDloss()
     model.compile(optimizer=opt, loss=ssd_loss.loss)
@@ -41,57 +37,53 @@ def load_model():
     return model
 
 
-def main(batch_size=16, steps_per_epoch=128):
-    model = load_model()
-    model.summary()
+def main(batch_size=16, steps_per_epoch=128, batch_images=3):
+    ray.init(ignore_reinit_error=True)
 
-    def gen_data():
-        for group in da.get_group_imgs():
-            images, X = da.load_data(group)
+    try:
+        model = load_model()
+        model.summary()
 
-            for image_info, bboxes in zip(images, X):
-                data_generator = da.data_augmentation(image_info, bboxes)
+        def gen_data():
+            for group in da.get_group_imgs():
+                images, X = da.load_data(group)
+
+                batch = []
+                deq = deque(maxlen=batch_images)
 
                 while True:
-                    try:
-                        img, y = next(data_generator)
+                    for i, (image_info, bboxes) in enumerate(zip(images, X), 1):
+                        deq.append((image_info, bboxes))
+                        if i % batch_images == 0:
+                            futures = [da.async_data_augmentation.remote(i, b)
+                                       for i, b in deq]
+                            results = ray.get(futures)
 
-                        img = np.expand_dims(img, axis=0)
-                        y = np.expand_dims(y, axis=0)
+                            batch += [item for sublist in results for item in sublist]
 
-                        # normalize pixels
-                        yield ((img - np.mean(img)) / (np.std(img) + 1e-15)), y
-                    except StopIteration:
-                        break
+                            if len(batch) > batch_size:
+                                topk = batch[:batch_size]
+                                batch = batch[batch_size:]
 
-    def batch_gen_data():
-        batch_x, batch_y = None, None
+                                imgs = [k[0] for k in topk]
+                                ys = [k[1] for k in topk]
+                                batch_x = np.concatenate(imgs, axis=0)
+                                batch_y = np.concatenate(ys, axis=0)
 
-        while True:
-            g = gen_data()
+                                yield batch_x, batch_y
 
-            for i, (x, y) in enumerate(g):
-                if i % batch_size == 0:
-                    if batch_x is not None and batch_y is not None:
-                        yield batch_x, batch_y
+        # value of how many data augs are made over each image
+        data_aug_empirical = 6
+        # epochs = (num_images * data aug)/(steps_per_epoch * batch_size)
+        model.fit_generator(gen_data(),
+                            steps_per_epoch=steps_per_epoch,
+                            epochs=int((count_images() * data_aug_empirical)
+                                       / (steps_per_epoch * batch_size) + 1),
+                            workers=0)
 
-                    batch_x = x
-                    batch_y = y
-                    continue
-
-                batch_x = np.concatenate([batch_x, x], axis=0)
-                batch_y = np.concatenate([batch_y, y], axis=0)
-
-    # value of how many data augs are made over each image
-    data_aug_empirical = 10
-    # epochs = (num_images * data aug)/(steps_per_epoch * batch_size)
-    model.fit_generator(batch_gen_data(),
-                        steps_per_epoch=steps_per_epoch,
-                        epochs=int((count_images() * data_aug_empirical)
-                                   / (steps_per_epoch * batch_size) + 1),
-                        workers=0)
-
-    model.save_weights(content.DATAPATH + '/weights300vgg16.h5')
+        model.save_weights(content.DATAPATH + '/fullweights300vgg16.h5')
+    finally:
+        ray.shutdown()
 
 
 if __name__ == '__main__':
