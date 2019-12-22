9e36728e77aec71bd8556ae5931da7a4084ea663
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-12-22 05:22:07 -0300

    major refactor in generate data script

--- project/generate_data.py
+++ project/generate_data.py
@@ -3,39 +3,34 @@ import h5py
 import numpy as np
 import ray
 
-from itertools import zip_longest
+from itertools import zip_longest, cycle
 
 from tqdm import tqdm
 
 from project.utils import data_augmentation as da
-
-import project.download_content as content
 
 from multiprocessing import cpu_count
 
 import uuid
 
 # TRAIN_DATAPATH = os.path.join(content.DATAPATH, "MODEL", '39_classes_300x300.h5')
-TRAIN_DATAPATH = os.path.join("/media/external", '39_classes_300x300.h5')
+TRAIN_DATAPATH = os.path.join("/media/external", '39_classes_300x300')
 
 
-def save_dataset(batch_x, batch_y, file_ref, group, data_ref):
+def save_dataset(x, y, file_ref):
     id = uuid.uuid4()
 
-    file_ref[group].create_dataset(name=f"{id}-x",
-                                   data=batch_x,
-                                   dtype=np.float16,
-                                   compression='gzip',
-                                   compression_opts=9)
+    file_ref.create_dataset(name=f"{id}-x",
+                            data=x,
+                            dtype=np.float16,
+                            compression='gzip',
+                            compression_opts=9)
 
-    file_ref[group].create_dataset(name=f"{id}-y",
-                                   data=batch_y,
-                                   dtype=np.float16,
-                                   compression='gzip',
-                                   compression_opts=9)
-
-    data_ref.append([f"{id}-x".encode("ascii", "ignore"),
-                     f"{id}-y".encode("ascii", "ignore")])
+    file_ref.create_dataset(name=f"{id}-y",
+                            data=y,
+                            dtype=np.float16,
+                            compression='gzip',
+                            compression_opts=9)
 
 
 def grouper(iterable, n, fillvalue=None):
@@ -44,76 +39,49 @@ def grouper(iterable, n, fillvalue=None):
     return zip_longest(*args, fillvalue=fillvalue)
 
 
-def create_data(futures, file_ref, data_ref, group, batch_size, batch_remain):
-    while len(futures):
-        done_id, futures = ray.wait(futures)
-        results = ray.get(done_id[0])
+@ray.remote
+def save_data(items, file_ref):
+    processed = []
 
-        batch_remain += [item for sublist in results for item in sublist]
+    if items is None or len(items) < 1:
+        return
 
-        while len(batch_remain) > batch_size:
-            topk = batch_remain[:batch_size]
-            batch_remain = batch_remain[batch_size:]
+    for image_info, bboxes in items:
+        processed += da.data_augmentation(image_info, bboxes)
 
-            batch_x = np.concatenate([k[0] for k in topk], axis=0)
-            batch_y = np.concatenate([k[1] for k in topk], axis=0)
+    with h5py.File(file_ref, 'a') as f:
+        batch_x = np.concatenate([x for x, _ in processed], axis=0)
+        batch_y = np.concatenate([y for _, y in processed], axis=0)
 
-            save_dataset(batch_x, batch_y, file_ref, group, data_ref)
+        save_dataset(batch_x, batch_y, f)
+        # for x, y in processed:
+        #     save_dataset(x, y, f, group)
 
-    return futures, batch_remain
 
-
-def main(batch_size=8, batch_images=10):
+def main(batch_images=100):
     ray.init()
-    file_ref = h5py.File(TRAIN_DATAPATH, 'w')
 
-    group_size = 300000
-    batch_remain = []
-    data_ref = None
+    files = [f"{TRAIN_DATAPATH}_{i}.h5" for i in range(cpu_count())]
+
+    for f in files:
+        temp_f = h5py.File(f, 'w')
+        temp_f.close()
 
     try:
-        for main_group in da.get_group_imgs()[:20]:
-            print("#"*20, f"processing GROUP {main_group}", "#"*20)
-            images, X = da.load_data(main_group)
+        for group in da.get_group_imgs()[:20]:
+            print("#"*20, f"processing GROUP {group}", "#"*20)
+            images, X = da.load_data(group)
 
             futures = []
 
-            for i, items in tqdm(enumerate(
-                grouper(zip(images, X), batch_images)
+            for i, (items, f) in tqdm(enumerate(
+                zip(grouper(zip(images, X), batch_images), cycle(files)), 1
             )):
-                # print("iteration:", i, " -- with batch remain:", len(batch_remain))
-                if i % group_size == 0:
-                    print("#"*20, f"creating SUB-GROUP {main_group}", "#"*20)
 
-                    if data_ref is None:
-                        data_ref = []
-                    group = str(int(i/group_size))
-                    file_ref.create_group(group)
+                futures += [save_data.remote(items, f)]
 
-                futures += [da.batch_data_augmentation.remote(items)]
-                futures += [da.batch_data_augmentation_2.remote(items)]
-                futures += [da.batch_data_augmentation_3.remote(items)]
-                if i % cpu_count() == 0:
-                    futures, batch_remain = create_data(futures, file_ref, data_ref,
-                                                        group, batch_size, batch_remain)
-
-                if (i+1) % group_size == 0:
-                    file_ref[group].create_dataset(name='data',
-                                                   shape=(len(data_ref), 2),
-                                                   data=data_ref,
-                                                   dtype=h5py.special_dtype(vlen=str),
-                                                   compression='gzip',
-                                                   compression_opts=9)
-                    data_ref = []
-
-            if len(data_ref) > 0:
-                file_ref[group].create_dataset(name='data',
-                                               shape=(len(data_ref), 2),
-                                               data=data_ref,
-                                               dtype=h5py.special_dtype(vlen=str),
-                                               compression='gzip',
-                                               compression_opts=9)
-                data_ref = []
+                if i % (cpu_count()) == 0:
+                    ray.get(futures)
 
     finally:
         ray.shutdown()
