95cdc5b6dca67adc9c215c4354bd322fb94df963
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2019-12-26 01:08:51 -0300

    updating ssd 300 full to work with 39+1 classes

--- project/model/ssd_model_300_full.py
+++ project/model/ssd_model_300_full.py
@@ -56,35 +56,21 @@ def ssd_model_300(reg=0.00003):
     conv11_2 = layers.Conv2D(256, (3, 3), strides=(1, 1), activation=activation, kernel_regularizer=l2(reg),
                              padding='valid', kernel_initializer=initializer, name='conv11_2')(conv11_1)
 
-    # conv12_1 = layers.Conv2D(128, (1, 1), activation=activation, padding='same',
-    #                          kernel_initializer=initializer, name='conv12_1')(conv11_2)
-    # conv12_2 = layers.Conv2D(256, (3, 3), strides=(1, 1), activation=activation,
-    #                          padding='valid', kernel_initializer=initializer, name='conv12_2')(conv12_1)
-
-    # conv13_1 = layers.Conv2D(128, (1, 1), activation=activation, padding='same',
-    #                          kernel_initializer=initializer, name='conv13_1')(conv12_2)
-    # conv13_2 = layers.Conv2D(256, (3, 3), strides=(1, 1), activation=activation,
-    #                          padding='valid', kernel_initializer=initializer, name='conv13_2')(conv13_1)
-
     # defining locs
-    loc_1 = layers.Conv2D(4 * 4, (3, 3), padding='same', name='1st_bbs')(conv4_3)
-    loc_2 = layers.Conv2D(6 * 4, (3, 3), padding='same', name='2nd_bbs')(fc7)
-    loc_3 = layers.Conv2D(6 * 4, (3, 3), padding='same', name='3rd_bbs')(conv8_2)
-    loc_4 = layers.Conv2D(6 * 4, (3, 3), padding='same', name='4th_bbs')(conv9_2)
-    loc_5 = layers.Conv2D(4 * 4, (3, 3), padding='same', name='5th_bbs')(conv10_2)
-    loc_6 = layers.Conv2D(4 * 4, (3, 3), padding='same', name='6th_bbs')(conv11_2)
-    # loc_7 = layers.Conv2D(5 * 4, (3, 3), padding='same', name='7th_bbs')(conv12_2)
-    # loc_8 = layers.Conv2D(5 * 4, (3, 3), padding='same', name='8th_bbs')(conv13_2)
+    loc_1 = layers.Conv2D(4 * 4, (1, 1), name='1st_bbs')(conv4_3)
+    loc_2 = layers.Conv2D(6 * 4, (1, 1), name='2nd_bbs')(fc7)
+    loc_3 = layers.Conv2D(6 * 4, (1, 1), name='3rd_bbs')(conv8_2)
+    loc_4 = layers.Conv2D(6 * 4, (1, 1), name='4th_bbs')(conv9_2)
+    loc_5 = layers.Conv2D(4 * 4, (1, 1), name='5th_bbs')(conv10_2)
+    loc_6 = layers.Conv2D(4 * 4, (1, 1), name='6th_bbs')(conv11_2)
 
     # defining confidences
-    conf_1 = layers.Conv2D(4 * 600, (3, 3), padding='same', name='1st_conf')(conv4_3)
-    conf_2 = layers.Conv2D(6 * 600, (3, 3), padding='same', name='2nd_conf')(fc7)
-    conf_3 = layers.Conv2D(6 * 600, (3, 3), padding='same', name='3rd_conf')(conv8_2)
-    conf_4 = layers.Conv2D(6 * 600, (3, 3), padding='same', name='4th_conf')(conv9_2)
-    conf_5 = layers.Conv2D(4 * 600, (3, 3), padding='same', name='5th_conf')(conv10_2)
-    conf_6 = layers.Conv2D(4 * 600, (3, 3), padding='same', name='6th_conf')(conv11_2)
-    # conf_7 = layers.Conv2D(5 * 2, (3, 3), padding='same', name='7th_conf')(conv12_2)
-    # conf_8 = layers.Conv2D(5 * 2, (3, 3), padding='same', name='8th_conf')(conv13_2)
+    conf_1 = layers.Conv2D(4 * 40, (1, 1), name='1st_conf')(conv4_3)
+    conf_2 = layers.Conv2D(6 * 40, (1, 1), name='2nd_conf')(fc7)
+    conf_3 = layers.Conv2D(6 * 40, (1, 1), name='3rd_conf')(conv8_2)
+    conf_4 = layers.Conv2D(6 * 40, (1, 1), name='4th_conf')(conv9_2)
+    conf_5 = layers.Conv2D(4 * 40, (1, 1), name='5th_conf')(conv10_2)
+    conf_6 = layers.Conv2D(4 * 40, (1, 1), name='6th_conf')(conv11_2)
 
     # reshapes
     rloc_1 = layers.Reshape((-1, 4), name='rloc1')(loc_1)
@@ -93,26 +79,27 @@ def ssd_model_300(reg=0.00003):
     rloc_4 = layers.Reshape((-1, 4), name='rloc4')(loc_4)
     rloc_5 = layers.Reshape((-1, 4), name='rloc5')(loc_5)
     rloc_6 = layers.Reshape((-1, 4), name='rloc6')(loc_6)
-    # rloc_7 = layers.Reshape((-1, 4), name='rloc7')(loc_7)
-    # rloc_8 = layers.Reshape((-1, 4), name='rloc8')(loc_8)
 
-    rconf_1 = layers.Reshape((-1, 600), name='rconf1')(conf_1)
-    rconf_2 = layers.Reshape((-1, 600), name='rconf2')(conf_2)
-    rconf_3 = layers.Reshape((-1, 600), name='rconf3')(conf_3)
-    rconf_4 = layers.Reshape((-1, 600), name='rconf4')(conf_4)
-    rconf_5 = layers.Reshape((-1, 600), name='rconf5')(conf_5)
-    rconf_6 = layers.Reshape((-1, 600), name='rconf6')(conf_6)
-    # rconf_7 = layers.Reshape((-1, 2), name='rconf7')(conf_7)
-    # rconf_8 = layers.Reshape((-1, 2), name='rconf8')(conf_8)
+    rconf_1 = layers.Reshape((-1, 40), name='rconf1')(conf_1)
+    rconf_1 = layers.Activation('softmax')(rconf_1)
+    rconf_2 = layers.Reshape((-1, 40), name='rconf2')(conf_2)
+    rconf_2 = layers.Activation('softmax')(rconf_2)
+    rconf_3 = layers.Reshape((-1, 40), name='rconf3')(conf_3)
+    rconf_3 = layers.Activation('softmax')(rconf_3)
+    rconf_4 = layers.Reshape((-1, 40), name='rconf4')(conf_4)
+    rconf_4 = layers.Activation('softmax')(rconf_4)
+    rconf_5 = layers.Reshape((-1, 40), name='rconf5')(conf_5)
+    rconf_5 = layers.Activation('softmax')(rconf_5)
+    rconf_6 = layers.Reshape((-1, 40), name='rconf6')(conf_6)
+    rconf_6 = layers.Activation('softmax')(rconf_6)
 
     locs = layers.Concatenate(axis=1, name='all_bbox')([rloc_1, rloc_2, rloc_3, rloc_4,
                                                         rloc_5, rloc_6])
 
     confs = layers.Concatenate(axis=1, name='all_preds')([rconf_1, rconf_2, rconf_3, rconf_4,
                                                           rconf_5, rconf_6])
-    confs_softmax = layers.Activation('softmax')(confs)
 
-    final = layers.Concatenate(axis=2)([confs_softmax, locs])
+    final = layers.Concatenate(axis=2)([confs, locs])
 
     model = Model(inputs=BASE_MODEL.input, output=[final])
 
