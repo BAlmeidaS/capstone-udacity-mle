3bafd598d53ca2fee60f07f07187199e89e3fc24
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2020-01-04 07:31:54 -0300

    getting images with ray in train to avoid gpu idle time

--- project/train_full.py
+++ project/train_full.py
@@ -12,6 +12,10 @@ from project.model.ssd_model_300_resnet import ssd_model_300_resnet
 from project.model.ssd_model_300_xception import ssd_model_300_xception
 from project.model.loss import SSDloss
 
+from concurrent.futures import ThreadPoolExecutor
+from concurrent import futures
+import ray
+
 import logging
 logging.getLogger().setLevel(logging.INFO)
 
@@ -41,6 +45,14 @@ def load_model(fn):
     return model
 
 
+@ray.remote
+def get_item(f_path, x_ref, y_ref):
+    with h5py.File(f_path, 'r') as f_temp:
+        X = f_temp[x_ref][:]
+        y = f_temp[y_ref][:]
+    return X, y
+
+
 def main(model_type, model_fn, batch_size=20, steps_per_epoch=200, batch_images=150):
     model = load_model(model_fn)
     model.summary()
@@ -49,16 +61,23 @@ def main(model_type, model_fn, batch_size=20, steps_per_epoch=200, batch_images=
 
     def gen_data():
         # each database with data preprocessed
-        files = [
-            "/media/external/all_classes_300x300_0.h5",
-            "/media/external/all_classes_300x300_1.h5",
-            "/media/external/all_classes_300x300_2.h5",
-            "/media/external/all_classes_300x300_3.h5",
+        files_path = [
             "/media/external/all_classes_300x300_4.h5",
-            "/media/external/all_classes_300x300_5.h5",
             "/media/external/all_classes_300x300_6.h5",
+            "/media/external/all_classes_300x300_1.h5",
+            "/media/external/all_classes_300x300_0.h5",
             "/media/external/all_classes_300x300_7.h5",
+            "/media/external/all_classes_300x300_3.h5",
+            "/media/external/all_classes_300x300_5.h5",
+            "/media/external/all_classes_300x300_2.h5",
         ]
+
+        files = []
+        for f_path in files_path:
+            with h5py.File(f_path, 'r') as f_temp:
+                batches = f_temp['batches'][:]
+                np.random.shuffle(batches)
+            files.append((f_path, batches))
 
         # this while true is to avoid keras error with generator functions
         while True:
@@ -68,35 +87,34 @@ def main(model_type, model_fn, batch_size=20, steps_per_epoch=200, batch_images=
             y = None
 
             # iterate in each file
-            for f_path in files:
-                f = h5py.File(f_path, 'r')
-                batches = f['batches'][:]
-                np.random.shuffle(batches)
+            results = []
+            for f_path, batches in files:
+                for x_ref, y_ref in batches:
+                    results.append(get_item.remote(f_path, x_ref, y_ref))
 
-                try:
-                    for x_ref, y_ref in batches:
-                        if X is None and y is None:
-                            # preload the first data ref
-                            X = f[x_ref][:]
-                            y = f[y_ref][:]
-                        else:
-                            # appending new X in old X
-                            X = np.concatenate([X, f[x_ref][:]], axis=0)
-                            y = np.concatenate([y, f[y_ref][:]], axis=0)
+                    if len(results) > 20:
+                        while len(results):
+                            done_id, results = ray.wait(results)
 
-                        while X.shape[0] > batch_size:
-                            # getting the first items in each tensor
-                            batch_x = X[:batch_size]
-                            batch_y = y[:batch_size]
+                            if X is None and y is None:
+                                X, y = ray.get(done_id[0])
+                            else:
+                                X_t, y_t = ray.get(done_id[0])
+                                X = np.concatenate([X, X_t], axis=0)
+                                y = np.concatenate([y, y_t], axis=0)
 
-                            # reduncing tensor size of axis -1
-                            X = X[batch_size:]
-                            y = y[batch_size:]
+                            while X.shape[0] > batch_size:
+                                # getting the first items in each tensor
+                                batch_x = X[:batch_size]
+                                batch_y = y[:batch_size]
 
-                            # yielding batch_x and batch_y to network training
-                            yield batch_x, batch_y
-                finally:
-                    f.close()
+                                # reduncing tensor size of axis -1
+                                X = X[batch_size:]
+                                y = y[batch_size:]
+
+                                # yielding batch_x and batch_y to network training
+                                yield batch_x, batch_y
+
 
     # hdf5 handle notebook explain this number
     total_images = 2550466
@@ -107,11 +125,15 @@ def main(model_type, model_fn, batch_size=20, steps_per_epoch=200, batch_images=
 
     callbacks = [lr_callback]
 
-    model.fit_generator(gen_data(),
-                        steps_per_epoch=steps_per_epoch,
-                        epochs=epochs,
-                        callbacks=callbacks,
-                        workers=0)
+    ray.init()
+    try:
+        model.fit_generator(gen_data(),
+                            steps_per_epoch=steps_per_epoch,
+                            epochs=epochs,
+                            callbacks=callbacks,
+                            workers=0)
+    finally:
+        ray.shutdown()
 
     model.save_weights(content.DATAPATH + f'/{model_type}-weights300vgg16.h5')
 
