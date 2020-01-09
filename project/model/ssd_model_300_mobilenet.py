ebf89a47b1afb11e9e236866b97158a48ca8d477
BAlmeidaS <bruno.almeida.silveira@gmail.com>
2020-01-09 06:18:53 -0300

    adding mode inference in mobilenet

--- project/model/ssd_model_300_mobilenet.py
+++ project/model/ssd_model_300_mobilenet.py
@@ -6,6 +6,9 @@ from keras.regularizers import l2
 
 from keras import layers
 
+from project.model.layers.anchorage import Anchorage
+from project.model.layers.supression import Suppression
+
 MNET = MobileNetV2(weights='imagenet', include_top=False, input_shape=(300, 300, 3))
 
 initializer = RandomUniform(minval=-1e-2, maxval=1e-2)
@@ -13,7 +16,7 @@ initializer = RandomUniform(minval=-1e-2, maxval=1e-2)
 activation = 'relu'
 
 
-def ssd_model_300_mobilenet(num_classes=600, reg=0.00003):
+def ssd_model_300_mobilenet(num_classes=600, reg=0.00003, inference=False):
     conv9_1 = layers.Conv2D(128, (1, 1), activation=activation, padding='same', kernel_regularizer=l2(reg),
                             kernel_initializer=initializer, name='conv9_1')(MNET.output)
     conv9_2 = layers.Conv2D(256, (3, 3), strides=(2, 2), activation=activation, padding='same', kernel_regularizer=l2(reg),
@@ -74,9 +77,9 @@ def ssd_model_300_mobilenet(num_classes=600, reg=0.00003):
 
     final = layers.Concatenate(axis=2)([confs, locs])
 
+    if inference:
+        anc = Anchorage(name='anchorage')(final)
+        final = Suppression(name='suppresion')(anc)
+
     model = Model(inputs=MNET.input, output=[final])
-
-    for l in model.layers[:14]:
-        l.trainable = True
-
     return model
